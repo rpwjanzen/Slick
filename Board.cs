41e972f0cb6617014a06bd6c87726089bb2ab27f
Ryan Janzen <rpwjanzen@gmail.com>
2009-10-21 15:38:14 -0600

    Added board

---
+++ Board.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Slick
+{
+    class Cell {
+        int PlayerOwner;
+        int OilDepth;
+        bool IsWater;
+
+        public Cell(int playerOwner, int oilDepth, bool isWater) {
+            this.PlayerOwner = playerOwner;
+            this.OilDepth = oilDepth;
+            this.IsWater = isWater;
+        }
+    }
+
+    class Board
+    {
+        public int Width;
+        public int Height;
+        public Cell[,] Cells;
+        public Random Random;
+
+        public Board(int width, int height, Random random)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.Random = random;
+
+            PopulateBoard();
+        }
+
+        void PopulateBoard()
+        {
+            Cells = new Cell[Width, Height];
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    Cells[x, y] = new Cell(0, Random.Next(0, 4), Random.NextDouble() > 0.5);
+                }
+            }
+        }
+    }
+}
--- Game1.cs
+++ Game1.cs
@@ -20,6 +20,8 @@ namespace Slick
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        const int BoardWidth = 1280;
+        const int BoardHeight = 720;
 
         public Game1()
         {
@@ -35,7 +37,8 @@ namespace Slick
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            var random = new Random(0);
+            var board = new Board(BoardWidth, BoardHeight, random);
 
             base.Initialize();
         }
@@ -52,20 +55,6 @@ namespace Slick
             // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
-        }
-
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
@@ -77,10 +66,6 @@ namespace Slick
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
